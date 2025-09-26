a7d8e25d256e3473378639a161a0c9866287654f
Josafat57 <124051549@upq.edu.mx>
2025-09-25 22:01:12 -0600

    Segundo ejercicio de tres
    En este segundo ejercicio se trata del uso de las promesas, el ejercicio nos pide que primero creemos una función llamada "verificarUsuario" que retorne una promesa.
    Si el nombre del usuario es ingresado como "admin" entonces la promesa responde con un "Acceso concedido", en caso contrario a que el nombre no sea "admin" se rechaza y se responde con un "Acceso denegado".

---
+++ Practica3/ejercicio2.js
@@ -0,0 +1,17 @@
+function verificarUsuario(usuario) {
+    return new Promise((resolve, reject) => {
+        if (usuario === "admin") {
+            resolve("Acceso concedido");
+        } else {
+            reject("Acceso denegado");
+        }
+    });
+}
+
+verificarUsuario("admin")
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
+
+verificarUsuario("Angel")
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
