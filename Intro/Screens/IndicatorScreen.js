2cb3c26c961896db8fe572647b301dfbbc6b7ca1
Josafat57 <124051549@upq.edu.mx>
2025-10-28 17:32:35 -0600

    Activity Indicator
    Esta es la actividad de la exposición "Activity Indicator", en esta práctica se nos enseña a cómo crear un círculo de carga (si mal no estoy así se llama), que a momento de presionar un botón se nos muestra la carga y luego una alerta. Sirve mucho para pantallas de carga.

--- Intro/Screens/IndicatorScreen.js
+++ Intro/Screens/IndicatorScreen.js
@@ -1,10 +1,47 @@
-import { Text, View } from 'react-native'
+import { Text, StyleSheet, View, Alert, Platform, ActivityIndicator, Button } from 'react-native'
+import {useState} from 'react'
 
 export default function IndicatorScreen() {
+  const [cargando, setCargando] = useState(false);
+  const carga =()=>{
+    setCargando(true);
+    setTimeout(()=> {
+      setCargando(false);
+      if (Platform.OS === 'web'){
+        window.alert('Carga completa');
+      } else {
+        Alert.alert('Carga completa');
+      }
+    }, 3000);
+  };
 
     return (
-      <View>
-        <Text>Próximamente...</Text>
+      <View style={styles.container}>
+        <Text style={styles.texto}>Presione para iniciar la carga</Text>
+        <View style={styles.botonesContainer}>
+          {cargando ? (<ActivityIndicator size='large' color='#9e28d4ff'></ActivityIndicator>) : (<Button color='#18e4f3ff' title='Presione para iniciar' onPress={carga}></Button>
+          )}
+        </View>
       </View>
     )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  texto: {
+    color: '#000000',
+    fontSize: 16,
+    fontFamily: 'Times New Roman',
+    fontWeight: 'bold',
+    fontStyle: 'italic',
+  },
+  botonesContainer: {
+    marginTop: 20,
+    gap: 20,
+  },
+})
