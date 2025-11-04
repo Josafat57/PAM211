5c70cd98dc7c8ca822a427128f8435f573e3c87b
Josafat57 <124051549@upq.edu.mx>
2025-11-04 04:00:09 -0600

    Otra vez
    Me di cuenta que el anterior código estaba mal, este ya es el ccorregido eso espero... Son las 4 de la mañana y tengo exmanes mañana, dios se apiade de mi

--- Intro/Screens/ImageScreen.js
+++ Intro/Screens/ImageScreen.js
@@ -1,74 +1,76 @@
-import { Text, StyleSheet, View, ImageBackground, Dimensions } from 'react-native'
-import React, { useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  ImageBackground,
+  Dimensions,
+} from 'react-native';
 
-const BackgroundImage = require ('../assets/19693-scaled.jpg');
-const BackgroundImage2 = require ('../assets/FF.jpg');
-export default function ImageScreen({navigation}){
-  const[showSplash, setShowSplash]=useState(true);
+const backgroundImage = require('../assets/splash-icon.png');
+
+export default function ImageBackGroundSplashScreen({ navigation }) {
+  const [showSplash, setShowSplash] = useState(true);
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setShowSplash(false);
     }, 3000);
-    return() => clearTimeout(timer)
-  },[]);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   if (showSplash) {
     return (
-    <View style={styles.container}>
-    <ImageBackground source={BackgroundImage}
-      style={styles.background}
-      resizeMode='cover'>
-
-      <View style={styles.overlay}>
-        <Text style={styles.tittle}>Bienvenido</Text>
-        <Text style={styles.tittle}>Cargando...</Text>
-      </View>
-    </ImageBackground>
-    </View>
-    
+      <ImageBackground
+        source={backgroundImage}
+        style={styles.background}
+        resizeMode="cover"
+      >
+        <View style={styles.overlay}>
+          <Text style={styles.title}>Bienvenido</Text>
+          <Text style={styles.title}>Cargando...</Text>
+        </View>
+      </ImageBackground>
     );
   }
 
   return (
-    <ImageBackground source={BackgroundImage2}
-      style={styles.background}
-      resizeMode='cover'>
     <View style={styles.mainScreen}>
-      <Text style={styles.mainText}>Bienvenido a la pantalla principal</Text>
-
-    </View></ImageBackground>
+      <Text style={styles.mainText}>Bienvenido a la Pantalla Principal</Text>
+    </View>
   );
 }
 
-const {width, height} = Dimensions.get('window');
-    const styles = StyleSheet.create({
-           background: {
-            width: width,
-            height: height,
-        },
-        overlay: {
-            flex: 1,
-//            backgroundColor: 'black',
-            justifyContent: 'center',
-            alignItems: 'center',
-            padding: 20,
-        },
-        tittle: {
-            fontSize: 32,
-            color: 'black',
-            fontWeight: 'bold',
-            marginBottom: 10,
-            textAlign: 'center',
-        },
-        mainScreen: {
-            flex: 1,
-            justifyContent: 'center',
-            alignItems: 'center',
-           // backgroundColor: 'white',
-        },
-        mainText: {
-            fontSize: 24,
-            color: 'white',
-        },
-    });
+const { width, height } = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+  background: {
+    width: width,
+    height: height,
+  },
+  overlay: {
+    flex: 1,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  title: {
+    color: '#fff',
+    fontSize: 32,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    textAlign: 'center',
+  },
+  mainScreen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f2f2f2',
+  },
+  mainText: {
+    fontSize: 24,
+    color: '#333',
+  },
+});
