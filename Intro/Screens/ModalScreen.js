3c132b064c38210ab2cf47f27667b4582b180332
Josafat57 <124051549@upq.edu.mx>
2025-11-04 04:21:06 -0600

    Proximamente
    Trabajo en progreso, estoy algo ocupado (aparte de que necesito dormir) y la verdad hice algo básico para no dejarlo con un texto pequeño y un fondo blanco predeterminado que te deja ciego.
    Un día esto tendrá contenido, un día de un futuro cercano espero...

--- Intro/Screens/ModalScreen.js
+++ Intro/Screens/ModalScreen.js
@@ -1,10 +1,46 @@
-import { Text, View } from 'react-native'
+import { Text, StyleSheet, View, Button, } from 'react-native';
+import React, { useState } from 'react';
+import MenuScreen from './MenuScreen';
 
 export default function ModalScreen() {
+   const [screen, setScreen] = useState('modal');
 
-    return (
-      <View>
-        <Text>Próximamente...</Text>
-      </View>
-    )
+   switch (screen) {
+     case 'menu': return <MenuScreen/>;
+     default:
+       return (
+         <View style={styles.background}>
+           <Text style={styles.title}>Próximamente...</Text>
+           <br/>
+           <Text style={styles.title}>En algún futuro cercano esto tendrá contenido...</Text>
+           <br/>
+           <View style={styles.buttonWrapper}>
+             <Button onPress={() => setScreen('menu')} title="Volver al menú" color="#000"/>
+           </View>
+          
+         </View>
+       );
+   }
 }
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#000000ff',
+  },
+  title: {
+    color: '#fff',
+    fontSize: 32,
+  },
+  buttonWrapper: {
+    backgroundColor: '#000',
+    borderWidth: 2,
+    borderColor: '#888',
+    borderRadius: 8,
+    marginBottom: 12,
+    width: 220,
+    overflow: 'hidden',
+  },
+});
