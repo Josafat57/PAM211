9be9f84451dbd36b467c020f084e5aa26a5e2352
Josafat57 <124051549@upq.edu.mx>
2025-11-06 19:51:35 -0600

    Primer commit
    Pues el primer commit del examen, no llevo mucho pero se progresa rápido eso espero.
    NO CARGA LA IMAGEN 😭

--- Examen2doP/App.js
+++ Examen2doP/App.js
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text, StyleSheet, View, ScrollView, ImageBackground} from 'react-native'
 
-export default function App() {
+const ImageBackground = require('../assets/fondo_restau.png')
+export default function Restaurante() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={StyleSheet.container}>
+      <ScrollView styles={styles.ScrollArea} contentContainerStyle={styles.scrollContent} showsVerticalScrollIndicator={true}>
+        <Text style={styles.subtitleWhite}>Restaurante Mexicano</Text>
+        <Text style={styles.subtitleWhite}>La mejor comida mexicana</Text>
+
+      </ScrollView>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({})
